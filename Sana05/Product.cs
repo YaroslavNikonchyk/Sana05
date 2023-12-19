6bac04f8d6710a2ec8a0ed4aa1bbc7bbcebbbc7a
nikocukaroslav <nikocukaroslav2006@gmail.com>
2023-12-19 18:57:09 +0200

    Added two new classes

---
+++ Sana05/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Sana05
+{
+    public class Product
+    {
+
+        protected string Name;
+        protected int Price;
+        Currency currency = new Currency("Dolar", 36.5);
+        protected int Quantity;
+        protected string Producer;
+        protected int weight;
+        public int GetPriceInUAH()
+        {
+
+        }
+    }
+}
