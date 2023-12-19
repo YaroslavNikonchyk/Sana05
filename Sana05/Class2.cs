3a85ca81e552fdcf5c7465c9d04b4481321eeff6
nikocukaroslav <nikocukaroslav2006@gmail.com>
2023-12-19 17:21:58 +0200

    Added class MyDate

---
+++ Sana05/Class2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sana05
+{
+    public class MyDate
+    {
+        protected int Year;
+        protected int Month;
+        protected int Day;
+        protected int Hours;
+        protected int Minutes;
+
+        public MyDate(int year, int month, int day, int hours, int minutes)
+        {
+            Year = year;
+            Month = month;
+            Day = day;
+            Hours = hours;
+            Minutes = minutes;
+        }
+    }
+}
