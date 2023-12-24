f92163324ebfe1bcffdf9ee45c637bc3be8e48ee
nikocukaroslav <nikocukaroslav2006@gmail.com>
2023-12-24 16:38:58 +0200

    added README.md

--- Sana05/Program.cs
+++ Sana05/Program.cs
@@ -4,11 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-      Currency currency = new Currency();
+        Currency currency = new Currency();
+        MyDate startDate = new MyDate(2023, 12, 07);
+        MyDate finishDate = new MyDate(2023, 12, 08);
         currency.SetName("Euro");
         currency.SetExRate(35.51);
         Console.WriteLine(currency.GetExRate());
-        Airplane airplane= new Airplane();
-        airplane
-    }
+        Airplane airplane = new Airplane("Varshava", "Kyiv", startDate, finishDate);
+        Console.WriteLine(airplane.GetFinishCity());
+    }    
 }
